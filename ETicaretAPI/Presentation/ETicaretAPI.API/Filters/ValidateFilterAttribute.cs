542b37c1b2f2ba76942d748f94cde85c4485742c
Serkan IŞIK <dehnas79@gmail.com>
2023-10-03 16:26:00 +0300

    FluentValidation - Filters - Custom response

---
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Filters/ValidateFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using ETicaretAPI.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ETicaretAPI.API.Filters
+{
+    public class ValidateFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+
+                context.Result = new BadRequestObjectResult(CustomResponseDto<NoContentDto>.Fail(400, errors));
+            }
+        }
+    }
+}
--- ETicaretAPI/Presentation/ETicaretAPI.API/Program.cs
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Program.cs
@@ -1,17 +1,23 @@
+using ETicaretAPI.API.Filters;
 using ETicaretAPI.API.Middlewares;
 using ETicaretAPI.Application;
 using ETicaretAPI.Application.Features.Commands.CarrierConfiguration.CreateCarrierConfiguration;
 using ETicaretAPI.Persistance;
 using FluentValidation.AspNetCore;
-
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddPersistanceServices();
 builder.Services.AddApplicationServices();
-builder.Services.AddControllers()
+builder.Services.AddControllers(options => options.Filters.Add(new ValidateFilterAttribute()))
     .AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<CreateCarrierConfigurationCommandValidator>());
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
