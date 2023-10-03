68eb22bfc0c0dc0d0aa2a13824bfc6382c4fd5b1
Serkan IŞIK <dehnas79@gmail.com>
2023-10-03 16:22:19 +0300

    FluentValidation

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Features/Commands/CarrierConfiguration/CreateCarrierConfiguration/CreateCarrierConfigurationCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.CarrierConfiguration.CreateCarrierConfiguration
+{
+    public class CreateCarrierConfigurationCommandValidator : AbstractValidator<CreateCarrierConfigurationCommandRequest>
+    {
+        public CreateCarrierConfigurationCommandValidator()
+        {
+            RuleFor(x => x.CarrierMaxDesi).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
+            RuleFor(x => x.CarrierMinDesi).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
+            RuleFor(x => x.CarrierCost).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
+            RuleFor(x => x.CarrierId).NotEmpty().WithMessage("{PropertyName} cannot be empty");
+        }
+    }
+}
--- ETicaretAPI/Presentation/ETicaretAPI.API/Program.cs
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Program.cs
@@ -1,13 +1,17 @@
 using ETicaretAPI.API.Middlewares;
 using ETicaretAPI.Application;
+using ETicaretAPI.Application.Features.Commands.CarrierConfiguration.CreateCarrierConfiguration;
 using ETicaretAPI.Persistance;
+using FluentValidation.AspNetCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddPersistanceServices();
 builder.Services.AddApplicationServices();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<CreateCarrierConfigurationCommandValidator>());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
